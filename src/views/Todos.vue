4aedd71f210f069fdc90eb1e965e251a6a607374
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-01-16 22:18:17 +0300

    features: - moved add item fetch query to Todos from AddTodo - fixed bug with empty load response from server - added filter todos by type all, completed, not completed

--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -6,10 +6,16 @@
         @add-todo="addTodo"
     ></add-todo>
     <hr>
+    <select v-model="filter">
+      <option value="all" selected>all</option>
+      <option value="completed">completed</option>
+      <option value="not-completed">not-completed</option>
+    </select>
+    <hr>
     <loader v-if="loading"></loader>
     <todo-list
-        v-else-if="todos.length > 0"
-        v-bind:todos="todos"
+        v-else-if="filteredTodos.length > 0"
+        v-bind:todos="filteredTodos"
         @remove-todo="removeTodo"
         @update-todo="updateTodo"
     >
@@ -28,7 +34,8 @@ export default {
   data() {
     return {
       todos: [],
-      loading: true
+      loading: true,
+      filter: 'all'
     }
   },
   mounted() {
@@ -37,23 +44,57 @@ export default {
         .then(json => {
           return Object.entries(json)
               .map(el => ({id: el[0], ...el[1]}))
-        })
+        }, () => [])
         .then(arr => {
           this.todos = arr
+        })
+        .finally(() => {
           this.loading = false
         })
+
   },
   components: {
     TodoList,
     AddTodo,
     Loader
   },
+  computed: {
+    filteredTodos() {
+      if (this.filter === 'completed') {
+        return this.todos.filter(i => i.completed)
+      } else if (this.filter === 'not-completed') {
+        return this.todos.filter(i => !i.completed)
+      } else {
+        return this.todos
+      }
+    }
+  },
   methods: {
     addTodo(todo) {
-      this.todos.push(todo)
+      fetch('https://todo-app-idm-default-rtdb.europe-west1.firebasedatabase.app/todo.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(todo),
+        cache: 'no-cache',
+        redirect: 'follow'
+      })
+          .then(response => response.json())
+          .then(json => {
+            if (json.name) {
+              todo.id = json.name
+              this.todos.push(todo)
+            }
+          })
     },
     removeTodo(id) {
       const idx = this.todos.findIndex(el => el.id === id)
+      fetch(`https://todo-app-idm-default-rtdb.europe-west1.firebasedatabase.app/todo/${id}.json`, {
+        method: 'DELETE',
+        cache: 'no-cache',
+        redirect: 'follow'
+      })
       if (idx >= 0) {
         this.todos.splice(idx, 1)
       }
